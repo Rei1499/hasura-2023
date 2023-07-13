ae5e19000740d2a3fa3d897f5e36c6f8945427fc
Inisi <102930364+Inisi@users.noreply.github.com>
2023-07-13 10:20:35 +0000

    changes that everyone did

--- react-badges/src/views/manager/IssueComponent.jsx
+++ react-badges/src/views/manager/IssueComponent.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import IssuingRequests from "./Issues/IssuingRequest";
+import { gql, useQuery } from "@apollo/client";
 
 const IssueComponent = () => {
-  const managerId = 2;
+  const GET_MANAGERS = gql`
+    query getManagersAndEngineers {
+      manager {
+        id
+        name
+      }
+      engineers {
+        id
+        name
+      }
+    }
+  `;
+
+  const r1 = useQuery(GET_MANAGERS);
+  const managerId = r1.data.manager.id;
   return (
     <>
       <IssuingRequests managerId={managerId} />
