5d503b3f7ee82d5daf9ef453a88dbf8d581828c1
Rei1499 <60137405+Rei1499@users.noreply.github.com>
2023-07-13 08:38:22 +0000

    Starting the Candidature Page

--- react-badges/src/views/manager/Candidatures.jsx
+++ react-badges/src/views/manager/Candidatures.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Box, Typography } from "@mui/material";
 
-const Candidatures = () =>{
+const Candidatures = () => {
+  return (
+    <>
+      <Box>
+        <Typography variant="h2" fontWeight="bold">
+          Candidatures
+        </Typography>
+        <Typography variant="h5" style={{ margin: "30px 0" }}>
+          Inside the following page you will find the full list of candidatures
+          that include you as the manager
+        </Typography>
 
-    return (
-        <>
-            <div>Candidatures</div>
-        </>
-    )
-}
+
+      </Box>
+    </>
+  );
+};
 
 export default Candidatures;
-
