c711a9a38964b2e40d1d7a48acd72db01e199e2b
Rei1499 <60137405+Rei1499@users.noreply.github.com>
2023-07-14 14:29:12 +0000

    Fixing errors

--- react-badges/src/views/manager/Proposals.jsx
+++ react-badges/src/views/manager/Proposals.jsx
@@ -1,16 +1,27 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import Table from "../../components/reUsable/Table";
 import { GET_PROPOSALS_WITH_STATUS } from "../../queries/CandidatureMutations";
+import { useNavigate } from "react-router-dom";
 
 const Proposals = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Box>
         <Typography variant={"h1"}>Proposals</Typography>
-        <Typography>Inside the following page you will find all the proposal created from you as well as the proposals that have asked your approval.</Typography>
-        <Table rows></Table>
-
+        <Typography>
+          Inside the following page you will find all the proposal created from
+          you as well as the proposals that have asked your approval.
+        </Typography>
+        {/* <Table rows></Table> */}
+        <Button
+          onClick={() => {
+            navigate("/proposalform");
+          }}
+        >
+          Create Proposal Form
+        </Button>
       </Box>
     </>
   );
